2c60515c04c1e94c1f92ee3a42110c01467c3466
Kai Franke <cosinushh@gmail.com>
2022-10-03 01:02:56 +0200

    add alt attribute to character images

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ function Card({ props, detailsState }) {
 
   return (
     <CardContainer>
-      <img src={props.image} alt="" />
+      <img src={props.image} alt={props.name} />
       <h2>{props.name}</h2>
       {details && (
         <DetailsList>
